bc0ca246d4dcdd81d79cae255eb465366e4fb21d
Kamran Bavar <kbavar@summit-tech.ca>
2024-05-14 12:00:51 -0400

    sentry v8 added
    Change-Id: I8fec6f7113abef4c01076659979b53ab1e33e868

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import * as dotenv from "dotenv"
 import "reflect-metadata"
 import { DirectorApi } from "./server"
+import * as Sentry from "@sentry/bun"
 
 // Load environment variables from .env file
 dotenv.config()
@@ -9,4 +10,9 @@ dotenv.config()
 const LISTEN_PORT = process.env.HTTP_PORT ? Number(process.env.HTTP_PORT) : 3000
 
 const directorApiServer = new DirectorApi()
+Sentry.init({
+  dsn: "https://4c1088a02529302c3c6b4d3a010dfb0b@sentry.erl.rcs.st/12",
+  // Performance Monitoring
+  tracesSampleRate: 1.0, // Capture 100% of the transactions
+})
 directorApiServer.startServer(LISTEN_PORT)
