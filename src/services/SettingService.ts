0d75a2d74b49036b0d63ea06c4e8845646f9ae1c
paniskina <paniskina@summit-tech.ca>
2025-02-06 14:27:04 -0500

    optimize system settings
    Change-Id: I5edf5e1be10354da1840f51883d74139b815f3f8

--- src/services/SettingService.ts
+++ src/services/SettingService.ts
@@ -9,26 +9,6 @@ export class SettingService {
     this.settingRepository = AppDataSource.getRepository(Setting)
   }
 
-  async getSystemSetting(settingName: string): Promise<unknown> {
-    const loadedSettings = await Setting.getLoadedSettings()
-    console.log(loadedSettings)
-    if (loadedSettings.has(settingName)) {
-      console.log(`Returning saved value: ${settingName}:${loadedSettings.get(settingName)}`)
-      return loadedSettings.get(settingName)
-    }
-    console.log(`fetching value for ${settingName} from DB`)
-    const settingObject = await this.settingRepository.findOne({
-      where: { key: settingName, configurableType: IsNull(), configurableId: IsNull() },
-    })
-    const value = settingObject ? settingObject.value : null
-
-    if (value) {
-      loadedSettings.set(settingName, value)
-      return value
-    }
-    return null
-  }
-
   async getSystemSettings(): Promise<any> {
     const settingName = Setting.SYSTEM_SETTINGS;
     const loadedSettings = await Setting.getLoadedSettings()
