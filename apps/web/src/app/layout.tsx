068a5585471aa3cf2468b3799c6ef9f8b0b714a0
Neil Fisher <nfisher@summit-tech.ca>
2025-11-10 20:25:02 -0500

    add Suspense around layout

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "../index.css";
+import { Suspense } from "react";
 import Header from "@/components/header";
 import Providers from "@/components/providers";
 
@@ -19,7 +20,15 @@ export default function RootLayout({
         <Providers>
           <div className="grid h-svh grid-rows-[auto_1fr]">
             <Header />
-            {children}
+            <Suspense
+              fallback={
+                <div className="flex h-full w-full items-center justify-center">
+                  Loading...
+                </div>
+              }
+            >
+              {children}
+            </Suspense>
           </div>
         </Providers>
       </body>
