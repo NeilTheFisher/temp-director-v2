92a7ac829a34d6e7b12dd220b40c82ef5354c874
Neil Fisher <nfisher@summit-tech.ca>
2025-11-12 02:09:06 -0500

    remove base contract errors

--- packages/contracts/src/contract/base.ts
+++ packages/contracts/src/contract/base.ts
@@ -1,10 +1,3 @@
 import { oc } from "@orpc/contract";
-import z from "zod";
 
-export const base = oc.errors({
-  NOT_IMPLEMENTED: {
-    data: z.any(),
-    message: "Not implemented",
-    status: 500,
-  },
-});
+export const base = oc;
