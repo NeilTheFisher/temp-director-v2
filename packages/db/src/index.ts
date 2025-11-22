85475fc40b6052e42521ce4599a2bce36632296c
Neil Fisher <nfisher@summit-tech.ca>
2025-11-22 14:22:40 -0500

    add prisma db adapter

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,8 +1,15 @@
+import { PrismaMariaDb } from "@prisma/adapter-mariadb";
+import { env } from "../../config";
 import { PrismaClient } from "../prisma/generated/client";
 
+const adapter = new PrismaMariaDb(env.DATABASE_URL);
 const prisma = new PrismaClient({
+  adapter,
   log: ["query"],
-  accelerateUrl: "",
 });
+
+console.log("connecting to db");
+await prisma.$connect();
+console.log("connected to db");
 
 export default prisma;
