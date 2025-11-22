d680e710922338a434cd3a2afc1cfd89bdb53a13
Neil Fisher <nfisher@summit-tech.ca>
2025-11-22 14:34:34 -0500

    preconnect to db and redis

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,15 +1,22 @@
-import { PrismaMariaDb } from "@prisma/adapter-mariadb";
+import { PrismaMariaDb as PrismaMySQL } from "@prisma/adapter-mariadb";
+import { redis } from "bun";
 import { env } from "../../config";
 import { PrismaClient } from "../prisma/generated/client";
 
-const adapter = new PrismaMariaDb(env.DATABASE_URL);
+const adapter = new PrismaMySQL(env.DATABASE_URL);
 const prisma = new PrismaClient({
   adapter,
   log: ["query"],
 });
 
+console.log("connecting to redis");
+console.time("connected to redis");
+await redis.connect();
+console.timeEnd("connected to redis");
+
 console.log("connecting to db");
+console.time("connected to db");
 await prisma.$connect();
-console.log("connected to db");
+console.timeEnd("connected to db");
 
 export default prisma;
