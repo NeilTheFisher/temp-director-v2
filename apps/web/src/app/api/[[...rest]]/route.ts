b626b998209e659e263ed31025d624532b5e9581
Neil Fisher <nfisher@summit-tech.ca>
2025-11-22 17:15:01 -0500

    add pino logging; add arktype package

--- apps/web/src/app/api/[[...rest]]/route.ts
+++ apps/web/src/app/api/[[...rest]]/route.ts
@@ -1,4 +1,5 @@
 import { appRouter, createContext } from "@director_v2/api";
+import { experimental_ArkTypeToJsonSchemaConverter as ArkTypeToJsonSchemaConverter } from "@orpc/arktype";
 import { OpenAPIHandler } from "@orpc/openapi/fetch";
 import { OpenAPIReferencePlugin } from "@orpc/openapi/plugins";
 import { onError } from "@orpc/server";
@@ -11,7 +12,33 @@ import { ZodToJsonSchemaConverter } from "@orpc/zod/zod4";
 import type { NextRequest } from "next/server";
 // polyfill for CompressionStream https://github.com/oven-sh/bun/issues/1723
 import "@ungap/compression-stream/poly";
+import { LoggingHandlerPlugin } from "@orpc/experimental-pino";
 import { experimental_SmartCoercionPlugin as SmartCoercionPlugin } from "@orpc/json-schema";
+import pino from "pino";
+import pretty from "pino-pretty";
+
+const stream = pretty({
+  colorize: true,
+});
+const logger = pino({ level: "debug" }, stream);
+
+declare global {
+  var log: typeof logger;
+}
+globalThis.log = logger;
+
+const sharedPlugins = [
+  new CompressionPlugin(),
+  new LoggingHandlerPlugin({
+    logger,
+    generateId: ({ context }) =>
+      context.session
+        ? `user-${context.session?.user.id}_${crypto.randomUUID()}`
+        : crypto.randomUUID(),
+    logRequestResponse: true,
+    logRequestAbort: true,
+  }),
+];
 
 const rpcHandler = new RPCHandler(appRouter, {
   interceptors: [
@@ -20,17 +47,20 @@ const rpcHandler = new RPCHandler(appRouter, {
     }),
   ],
   plugins: [
-    new CompressionPlugin(),
+    ...sharedPlugins,
 
     new SimpleCsrfProtectionHandlerPlugin(),
     new StrictGetMethodPlugin(),
   ],
 });
 
-const schemaConverters = [new ZodToJsonSchemaConverter()];
+const schemaConverters = [
+  new ZodToJsonSchemaConverter(),
+  new ArkTypeToJsonSchemaConverter(),
+];
 const apiHandler = new OpenAPIHandler(appRouter, {
   plugins: [
-    new CompressionPlugin(),
+    ...sharedPlugins,
 
     new OpenAPIReferencePlugin({
       schemaConverters,
