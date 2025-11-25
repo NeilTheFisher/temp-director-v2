4083eb660b5fdc5e15f7a66254482ea3a3e4537d
Neil Fisher <nfisher@summit-tech.ca>
2025-11-25 15:49:32 -0500

    rm clients from git repo (should be auto generated using the templates in the future)

--- packages/api/openapi/clients/kotlin/src/main/kotlin/director/v2/client/infrastructure/RequestMethod.kt
+++
@@ -1,8 +0,0 @@
-package director.v2.client.infrastructure
-
-/**
- * Provides enumerated HTTP verbs
- */
-enum class RequestMethod {
-    GET, DELETE, HEAD, OPTIONS, PATCH, POST, PUT
-}
--- packages/api/openapi/clients/kotlin/src/main/kotlin/director/v2/client/models/AnyOfLessThanGreaterThan.kt
+++
@@ -1,6 +0,0 @@
-package director.v2.client.models
-
-/**
- * Type alias for AnyOf schemas
- */
-typealias AnyOfLessThanGreaterThan = Any?
