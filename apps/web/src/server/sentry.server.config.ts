e7dc8a7cd04ac83eef5520b47eb0c968b9849979
Neil Fisher <nfisher@summit-tech.ca>
2025-11-26 13:41:46 -0500

    manual otel setup using `skipOpenTelemetrySetup`

--- apps/web/src/server/sentry.server.config.ts
+++ apps/web/src/server/sentry.server.config.ts
@@ -1,37 +1,23 @@
 import { env } from "@director_v2/config";
+import { SpanStatusCode, trace } from "@opentelemetry/api";
 import { getNodeAutoInstrumentations } from "@opentelemetry/auto-instrumentations-node";
 import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-http";
-import { SimpleSpanProcessor } from "@opentelemetry/sdk-trace-node";
+import { resourceFromAttributes } from "@opentelemetry/resources";
+import { NodeSDK } from "@opentelemetry/sdk-node";
+import {
+  BatchSpanProcessor,
+  SimpleSpanProcessor,
+} from "@opentelemetry/sdk-trace-node";
 import { ORPCInstrumentation } from "@orpc/otel";
 import { PrismaInstrumentation } from "@prisma/instrumentation";
 import * as Sentry from "@sentry/node";
+import {
+  SentryPropagator,
+  SentrySampler,
+  SentrySpanProcessor,
+} from "@sentry/opentelemetry";
 
-// class CustomOTLPTraceExporter extends OTLPTraceExporter {
-//   // biome-ignore lint/suspicious/noExplicitAny: don't feel like doing `import { ExportResult } from '@opentelemetry/core';`
-//   export(spans: ReadableSpan[], resultCallback: (result: any) => void): void {
-//     // fix span name for Next.js API routes like /api/[[...rest]]
-//     // readonly span but writing it anyway works
-//     for (const span of spans) {
-//       if (!span.attributes["http.target"]) continue;
-//       Object.assign(span, { name: span.attributes["http.target"] });
-//     }
-
-//     super.export(spans, resultCallback);
-//   }
-// }
-
-function getDevSpanProcessors() {
-  if (env.ENV !== "development") return [];
-
-  const OTEL_TRACE_EXPORTER_URL =
-    process.env.OTEL_TRACE_EXPORTER_URL || "http://localhost:4318/v1/traces";
-  const traceExporter = new OTLPTraceExporter({
-    url: OTEL_TRACE_EXPORTER_URL,
-  });
-  return [new SimpleSpanProcessor(traceExporter)];
-}
-
-Sentry.init({
+const sentryClient = Sentry.init({
   dsn: env.SENTRY_DSN,
   environment: env.ENV,
   serverName: "director-api",
@@ -42,14 +28,68 @@ Sentry.init({
   sendDefaultPii: env.ENV === "development",
 
   defaultIntegrations: false,
-  openTelemetryInstrumentations: [
-    ...getNodeAutoInstrumentations({
+  skipOpenTelemetrySetup: true,
+});
+
+const OTEL_TRACE_EXPORTER_URL =
+  process.env.OTEL_TRACE_EXPORTER_URL || "http://localhost:4318/v1/traces";
+
+const traceExporter = new OTLPTraceExporter({
+  url: OTEL_TRACE_EXPORTER_URL,
+});
+
+const otelSdk = new NodeSDK({
+  resource: resourceFromAttributes({
+    "service.name": "director",
+  }),
+  sampler: sentryClient ? new SentrySampler(sentryClient) : undefined,
+  spanProcessors: [
+    process.env.NODE_ENV === "development"
+      ? new SimpleSpanProcessor(traceExporter)
+      : new BatchSpanProcessor(traceExporter),
+    new SentrySpanProcessor(),
+  ],
+  instrumentations: [
+    getNodeAutoInstrumentations({
       "@opentelemetry/instrumentation-http": {
         enabled: false,
+      },
+      "@opentelemetry/instrumentation-ioredis": {
+        enabled: true,
+        requireParentSpan: false,
       },
     }),
     new ORPCInstrumentation(),
     new PrismaInstrumentation(),
   ],
-  openTelemetrySpanProcessors: [...getDevSpanProcessors()],
+  textMapPropagator: new SentryPropagator(),
+  contextManager: new Sentry.SentryContextManager(),
 });
+
+otelSdk.start();
+
+const tracer = trace.getTracer("uncaught-errors");
+
+function recordError(eventName: string, reason: unknown) {
+  const span = tracer.startSpan(eventName);
+  const message = String(reason);
+
+  if (reason instanceof Error) {
+    span.recordException(reason);
+  } else {
+    span.recordException({ message });
+  }
+
+  span.setStatus({ code: SpanStatusCode.ERROR, message });
+  span.end();
+}
+
+process.on("uncaughtException", (reason) => {
+  recordError("uncaughtException", reason);
+});
+
+process.on("unhandledRejection", (reason) => {
+  recordError("unhandledRejection", reason);
+});
+
+Sentry.validateOpenTelemetrySetup();
